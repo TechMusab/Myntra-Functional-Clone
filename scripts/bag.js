ca6529ef216df2791da76d9d9bf2f559625acb6c
Musab Bin Ubaid <joiya4636@gmail.com>
2024-06-18 15:57:28 -0700

    Added some more functionality

--- scripts/bag.js
+++ scripts/bag.js
@@ -1,35 +1,57 @@
-
+let obj;
 onload();
 
 function onload(){
+    loadBagItemsobjects();
     displayBagItems();
+}
+function loadBagItemsobjects(){
+   obj= bagarray.map(itemid=>{
+        for(let i=0;i<items.length;i++){
+            if(itemid==items[i].id){
+                return items[i];// Npw the concept is clear . it is seraching each item id in bag with every id in items and returning the matching item based on item id
+            }
+        }
 
+    })
 }
 function displayBagItems(){
     let container=document.querySelector('.bag-items-container');
-    container.innerHTML=`
-    <div class="bag-item-container">
-    <div class="item-left-part">
-      <img class="bag-item-img" src="../images/4.jpg">
-    </div>
-    <div class="item-right-part">
-      <div class="company">ADIDAS</div>
-      <div class="item-name">Men Printed Polo Collar Indian Cricket ODI Jersey</div>
-      <div class="price-container">
-        <span class="current-price">Rs 999</span>
-        <span class="original-price">Rs 999</span>
-        <span class="discount-percentage">(0% OFF)</span>
-      </div>
-      <div class="return-period">
-        <span class="return-period-days">14 days</span> return available
-      </div>
-      <div class="delivery-details">
-        Delivery by
-        <span class="delivery-details-days">10 Oct 2023</span>
-      </div>
-    </div>
-
-    <div class="remove-from-cart">X</div>
+    let innerhtml='';
+    obj.forEach(bagitem => {
+        innerhtml+=generateitemhtml(bagitem);
+    });
+    container.innerHTML=innerhtml;
+}
+function removeFromBag(itemid){
+bagarray=bagarray.filter(bagitemid=>bagitemid!=itemid);
+localStorage.setItem('bagarray', JSON.stringify(bagarray));
+loadBagItemsobjects();
+displaycount();
+displayBagItems();
+}
+function generateitemhtml(item){
+return    `<div class="bag-item-container">
+<div class="item-left-part">
+  <img class="bag-item-img" src="../${item.image}">
+</div>
+<div class="item-right-part">
+  <div class="company">${item.company}</div>
+  <div class="item-name">${item.item_name}</div>
+  <div class="price-container">
+    <span class="current-price"> ${item.current_price}</span>
+    <span class="original-price">${item.original_price}</span>
+    <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
   </div>
-    `
+  <div class="return-period">
+    <span class="return-period-days">${item.return_period}</span> return available
+  </div>
+  <div class="delivery-details">
+    Delivery by
+    <span class="delivery-details-days">${item.delivery_date}</span>
+  </div>
+</div>
+
+<div class="remove-from-cart" onclick="removeFromBag(${item.id});">X</div>
+</div>`
 }
